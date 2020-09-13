fbd8368204dc065dff6873db871cc64b49da02c0
Axenov97 <60551102+Axenov97@users.noreply.github.com>
2020-09-14 00:43:22 +0300

    Add files via upload

---
+++ js/load.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+	$('.preloader__logo').addClass('active');
+};
+window.setTimeout(function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 1000); 	
+  },2500);
+
+$( document ).ready(function() {
+	$('.header-burger').click(function(event){
+		$('.header-burger, .menu').toggleClass('active');
+	});
+
+	$('.down_menu').parallax();
+});
