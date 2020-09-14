a672e6c7ae22ce7bb121de40ebc51204df974349
Axenov97 <dimka180497@gmail.com>
2020-09-14 17:48:45 +0300

    anim

--- js/load.js
+++ js/load.js
@@ -7,10 +7,17 @@ window.setTimeout(function () {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
     }, 1000); 	
-  },2500);
+    window.setTimeout(function () {
+  $('.ship').addClass('active');    
+  },200);
+    window.setTimeout(function () {
+  $('.plane').addClass('active');    
+  },500);
+},2500);
 
 $( document ).ready(function() {
 	$('.header-burger').click(function(event){
 		$('.header-burger, .menu').toggleClass('active');
 	});
+
 });
