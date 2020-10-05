feb11a4732f1e884257fa119ff384fc7783ca8fd
Axenov97 <dimka180497@gmail.com>
2020-10-05 20:11:42 +0300

    s

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 	$('.parallax__list>li').addClass('layer');
 	$('.parallax__list').parallax();
-	$('.wrapper').addClass('active');
+	
 	setTimeout()
 });
 
   window.onload = function () {
+
 	setTimeout(()=>{
+	$('.wrapper').addClass('active');
 		document.querySelector('.header').classList.add('active');
 		document.querySelector('.menu').classList.add('active');
 		document.querySelector('.parallax__title').classList.add('active');
